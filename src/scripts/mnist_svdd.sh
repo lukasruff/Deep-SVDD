0161b2730c4d5071af360264e86b660aa828ee16
lukasruff <lukas.ruff@gmail.com>
2018-11-13 17:25:39 +0100

    Changed MNIST Deep SVDD script such that network has no bias terms.

--- src/scripts/mnist_svdd.sh
+++ src/scripts/mnist_svdd.sh
@@ -19,5 +19,5 @@ python baseline.py --dataset mnist --solver $solver --loss svdd --lr $lr --lr_dr
     --seed $seed --lr_drop_factor 10 --block_coordinate $block_coordinate --R_update_solver minimize_scalar \
     --R_update_scalar_method bounded --R_update_lp_obj primal --use_batch_norm 1 --pretrain $pretrain \
     --batch_size 200 --n_epochs $n_epochs --device $device --xp_dir $xp_dir --leaky_relu 1 --weight_decay 1 --C 1e6 \
-    --hard_margin $hard_margin --nu 0.1 --weight_dict_init 1 --unit_norm_used l1 --gcn 1 --mnist_rep_dim 32 \
-    --mnist_normal $mnist_normal --mnist_outlier $mnist_outlier;
+    --hard_margin $hard_margin --nu 0.1 --weight_dict_init 1 --unit_norm_used l1 --gcn 1 --mnist_bias 0 \
+    --mnist_val_frac 0 --mnist_rep_dim 32 --mnist_normal $mnist_normal --mnist_outlier $mnist_outlier;
